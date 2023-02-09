183180aadb4d94b1006544ef8b7dea37855a6982
raha-nug <rival10221029@student.sttcipasung.ac.id>
2023-02-10 04:59:26 +0700

    add new DOM Selector

--- script.js
+++ script.js
@@ -3,14 +3,23 @@
 //document.getElementById()
 //mengembalikan element
 
-const judul = document.getElementById('judul');
-    judul.style.color = 'red';
-    judul.innerHTML = 'Rival Nugraha';
+const judul = document.getElementById('judul')
+    judul.style.color = 'red'
+    judul.innerHTML = 'Rival Nugraha'
 
 
 //document.getElementsByTagName()
 //mengembalikan HTMLCollection
 
-const p = document.getElementsByTagName('p');
+const p = document.getElementsByTagName('p')
 
-p[0].style.backgroundColor = 'lightblue'
+for(let i = 0; i < p.length; i++){
+    p[i].style.backgroundColor = 'lightblue'
+}
+
+
+//document.getElementsByClassName()
+//mengembalikan HTMLCollection
+
+const p1 = document.getElementsByClassName('p1')
+p1[0].innerHTML = 'Ini dirubah dari javascript'
